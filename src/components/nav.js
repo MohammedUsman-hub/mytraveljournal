034f2fa40aa32eb183ae0d8d7bb58badfc29980a
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-01-09 12:16:56 +0000

    adding nav.js

---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "../index.css"
+
+export default class Nav extends Component{
+    render(){
+        return(
+            <nav>
+                <img id="earth-icon" src='images/earth.png'></img>
+            </nav>
+        )
+    }
+}
