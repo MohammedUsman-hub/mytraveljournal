1713f3076daa2656115cdc090d177257ab03058c
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-01-16 22:17:29 +0000

    nav bar complete

--- src/component/nav.js
+++ src/component/nav.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import EarthIcon from "../images/earth.png"
 import "../index.css"
 
 export default class Nav extends Component{
     render(){
         return(
-            <nav>
-                <img id="earth-icon" src='images/earth.png'></img>
+            <nav className='nav-bar'>
+                <img id="earth-icon" src={EarthIcon}></img>
+                <h1 className='nav-h1'>My Travel Journal</h1>
             </nav>
         )
     }
