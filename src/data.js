6dd9766ed2fc784bcbc7c1f2da8b857a617f229a
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-01-16 22:23:24 +0000

    adding data to data.js

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export default[
+    {
+        id: 1,
+        country: "Japan",
+        location: "Akihabara, Tokyo",
+        description:"anime",
+        date:"14th Jan 2022 - 24 Jan 2022",
+        googlemaps: "View on Google Maps"
+
+    },
+]
