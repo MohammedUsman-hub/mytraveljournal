181367fc210baa5603e53912a57447ce27e35cdc
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-01-19 01:42:57 +0000

    updating contact.js

--- src/contact.js
+++ src/contact.js
@@ -5,8 +5,21 @@ export default function Contact(props){
     return(
         <div className='contacts'>
             <div className='contact-card'>
+                
                 <img src={props.img}></img>
-                <h3>{props.country}</h3>
+                <h3>{props.location}</h3>
+
+                <div>
+                    <img src="images/location-icon.png"></img>
+                    <h3>{props.country}</h3>
+                    <a>{props.googlemaps}</a>
+                </div>
+
+                <div>
+                    <h5>{props.date}</h5>
+                    <h5>{props.description}</h5>
+                </div>
+                
             </div>
         </div>
     )
