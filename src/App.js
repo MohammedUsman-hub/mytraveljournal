aa89dea02c4af1a4ff1d04d18a26f409e113b3f3
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-01-19 02:07:04 +0000

    images in public App.js updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react"
+
 import Nav from "./component/nav"
+import CountryInfo from "./countryinfo"
+
+import data from "./data"
+
 
 export default function App(){
+    const DataJs = data.map(item => {
+        return(
+            <CountryInfo
+                key={item.id}
+                item={item}
+            />
+        )
+    })
     return(
         <div>
             <Nav/>
+            <section>
+                {DataJs}
+            </section>
         </div>
         
     )
--- src/countryinfo.js
+++ src/countryinfo.js
@@ -7,8 +7,8 @@ export default function CountryInfo(props){
             <img className='' src={"./images/${props.item.img}"}></img>
             <div>
                 <img className='' src='./images/location-icon.png'></img>
-                <span>{span.country}</span>
-                <span>{props.location}</span>
+                <h4>{props.country}</h4>
+                <h4>{props.location}</h4>
                 <h4>{props.date}</h4>
                 <h4>{props.description}</h4>
             </div>
