cc7a5b741702420ea114c1230583b1a6ce6994db
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-01-20 22:29:15 +0000

    changes img call (fold in pub)

--- src/component/nav.js
+++ src/component/nav.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import EarthIcon from "../images/earth.png"
 import "../index.css"
 
 export default class Nav extends Component{
     render(){
         return(
             <nav className='nav-bar'>
-                <img id="earth-icon" src={EarthIcon}></img>
+                <img id="earth-icon" src="./images/earth.png"></img>
                 <h1 className='nav-h1'>My Travel Journal</h1>
             </nav>
         )
