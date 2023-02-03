291b1971f0c9b63916a7eea2d050725cd34fbffa
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-02-03 22:36:43 +0000

    adding to css, and classes 2 countryinfo.js

--- src/countryinfo.js
+++ src/countryinfo.js
@@ -3,15 +3,15 @@ import "./index.css"
 
 export default function CountryInfo(props){
     return(
-        <div>
+        <div className='countryInfo'>
             <img className='' src={"./images/${props.item.img}"}></img>
             <div>
-                <img className='' src={"./images/${props.item.imgIcon}"}></img>
-                <h4>{props.country}</h4>
-                <h4>{props.location}</h4>
-                <h4>{props.date}</h4>
-                <h4>{props.description}</h4>
-                <h4>{props.googlemaps}</h4>
+                <img className='county-img' src={"./images/${props.item.imgIcon}"}></img>
+                <span>{props.country}</span>
+                <span>{props.location}</span>
+                <span>{props.date}</span>
+                <span>{props.description}</span>
+                <span>{props.googlemaps}</span>
             </div>
         </div>
     )
