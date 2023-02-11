41c5a038b37bfa080604575ae3d8b6ad0e93e3de
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-02-11 02:02:14 +0000

    styling countryinfo.js

--- src/countryinfo.js
+++ src/countryinfo.js
@@ -4,9 +4,9 @@ import "./index.css"
 export default function CountryInfo(props){
     return(
         <div className='countryInfo'>
-            <img className='county-img' src={`./images/${props.item.img}`}></img>
+            <img className='country-img' src={`./images/${props.item.img}`}></img>
             <div className='country-stats'>
-                <img className='county-img' src={`./images/${props.item.imgIcon}`}></img>
+                <img className='location-img' src={`./images/${props.item.imgIcon}`}></img>
                 <span>{props.item.country}</span>
                 <span>{props.item.location}</span>
                 <span>{props.item.date}</span>
