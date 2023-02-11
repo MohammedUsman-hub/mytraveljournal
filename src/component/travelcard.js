69a0edcb5d7cc30f82275ff4c2723b534aaca853
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-02-11 01:12:01 +0000

    adding code to travelcard.js

--- src/component/travelcard.js
+++ src/component/travelcard.js
@@ -3,8 +3,11 @@ import "../index.css"
 
 export default function travelCard(props){
     return(
-        <div>
-            
+        <div className='travelCard'>
+            <img className='travelCard-img' src={`../images/${props.item.img}`}></img>
+                <div className='travelCard-stats'>
+                    <img className='travelCard-location-icon' src={process.env.PUBLIC_URL + 'images/location-icon.png'}></img>
+                </div>
         </div>
     )
 }
