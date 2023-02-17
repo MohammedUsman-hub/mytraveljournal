7e3e1fef44571bc3b11f38fdd4026e8d877feb10
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-02-17 21:43:54 +0000

    styling fin? Maybe?

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export default[
         location: "Akihabara, Tokyo",
         img:"tokyo.jpg",
         imgIcon: "location-icon.png",
-        description:"anime",
+        description:"Akihabara is a buzzing shopping hub famed for its electronics retailers, ranging from tiny stalls to vast department stores like Yodobashi Multimedia Akiba. Venues specializing in manga, anime, and video games include Tokyo Anime Center, for exhibits and souvenirs, and Radio Kaikan with 10 floors of toys, trading cards, and collectibles. Staff dressed as maids or butlers serve tea and desserts at nearby maid cafes.",
         date:"14th Jan 2022 - 24 Jan 2022",
         googlemaps: "View on Google Maps"
     },
