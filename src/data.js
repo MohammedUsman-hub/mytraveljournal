2af4269386c73ae92c243a1c0992d1ca16c5c582
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-02-17 21:46:55 +0000

    finish!

--- src/data.js
+++ src/data.js
@@ -9,4 +9,26 @@ export default[
         date:"14th Jan 2022 - 24 Jan 2022",
         googlemaps: "View on Google Maps"
     },
+
+    {
+        id: 1,
+        country: "Japan",
+        location: "Akihabara, Tokyo",
+        img:"tokyo.jpg",
+        imgIcon: "location-icon.png",
+        description:"Akihabara is a buzzing shopping hub famed for its electronics retailers, ranging from tiny stalls to vast department stores like Yodobashi Multimedia Akiba. Venues specializing in manga, anime, and video games include Tokyo Anime Center, for exhibits and souvenirs, and Radio Kaikan with 10 floors of toys, trading cards, and collectibles. Staff dressed as maids or butlers serve tea and desserts at nearby maid cafes.",
+        date:"14th Jan 2022 - 24 Jan 2022",
+        googlemaps: "View on Google Maps"
+    },
+
+    {
+        id: 1,
+        country: "Japan",
+        location: "Akihabara, Tokyo",
+        img:"tokyo.jpg",
+        imgIcon: "location-icon.png",
+        description:"Akihabara is a buzzing shopping hub famed for its electronics retailers, ranging from tiny stalls to vast department stores like Yodobashi Multimedia Akiba. Venues specializing in manga, anime, and video games include Tokyo Anime Center, for exhibits and souvenirs, and Radio Kaikan with 10 floors of toys, trading cards, and collectibles. Staff dressed as maids or butlers serve tea and desserts at nearby maid cafes.",
+        date:"14th Jan 2022 - 24 Jan 2022",
+        googlemaps: "View on Google Maps"
+    },
 ]
