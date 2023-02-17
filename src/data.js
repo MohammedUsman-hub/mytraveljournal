864fa7fa14792965f996ca915b0dc2bd0dfc0702
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-02-17 22:10:52 +0000

    okay now its fin!

--- src/data.js
+++ src/data.js
@@ -6,29 +6,29 @@ export default[
         img:"tokyo.jpg",
         imgIcon: "location-icon.png",
         description:"Akihabara is a buzzing shopping hub famed for its electronics retailers, ranging from tiny stalls to vast department stores like Yodobashi Multimedia Akiba. Venues specializing in manga, anime, and video games include Tokyo Anime Center, for exhibits and souvenirs, and Radio Kaikan with 10 floors of toys, trading cards, and collectibles. Staff dressed as maids or butlers serve tea and desserts at nearby maid cafes.",
-        date:"14th Jan 2022 - 24 Jan 2022",
-        googlemaps: "View on Google Maps"
+        date:"14th Jan 2022 - 24th Jan 2022",
+        
     },
 
     {
-        id: 1,
-        country: "Japan",
-        location: "Akihabara, Tokyo",
-        img:"tokyo.jpg",
+        id: 2,
+        country: "Pakistan",
+        location: "Lahore",
+        img:"lahore.jpg",
         imgIcon: "location-icon.png",
-        description:"Akihabara is a buzzing shopping hub famed for its electronics retailers, ranging from tiny stalls to vast department stores like Yodobashi Multimedia Akiba. Venues specializing in manga, anime, and video games include Tokyo Anime Center, for exhibits and souvenirs, and Radio Kaikan with 10 floors of toys, trading cards, and collectibles. Staff dressed as maids or butlers serve tea and desserts at nearby maid cafes.",
-        date:"14th Jan 2022 - 24 Jan 2022",
-        googlemaps: "View on Google Maps"
+        description:"Lahore is the second most populous city in Pakistan after Karachi and 26th most populous city in the world, with a population of over 13 million. It is the capital of the province of Punjab where it is the largest city.",
+        date:"1st May 2023 - 10th May 2023",
+        
     },
 
     {
-        id: 1,
-        country: "Japan",
-        location: "Akihabara, Tokyo",
-        img:"tokyo.jpg",
+        id: 3,
+        country: "England",
+        location: "London",
+        img:"london.jpg",
         imgIcon: "location-icon.png",
-        description:"Akihabara is a buzzing shopping hub famed for its electronics retailers, ranging from tiny stalls to vast department stores like Yodobashi Multimedia Akiba. Venues specializing in manga, anime, and video games include Tokyo Anime Center, for exhibits and souvenirs, and Radio Kaikan with 10 floors of toys, trading cards, and collectibles. Staff dressed as maids or butlers serve tea and desserts at nearby maid cafes.",
-        date:"14th Jan 2022 - 24 Jan 2022",
-        googlemaps: "View on Google Maps"
+        description:"London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. Across the Thames River, the London Eye observation wheel provides panoramic views of the South Bank cultural complex, and the entire city.",
+        date:"14th Feb 2023 - 9th Mar 2022",
+        
     },
 ]
