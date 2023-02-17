d5c6024a58138b49e67ebefb86b478151f10c683
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-02-17 21:23:40 +0000

    getting closer to finishing styling

--- src/countryinfo.js
+++ src/countryinfo.js
@@ -8,7 +8,7 @@ export default function CountryInfo(props){
             <div className='country-stats'>
                 <img className='location-img' src={`./images/${props.item.imgIcon}`}></img>
                 <span className='country-name'><b>{props.item.country}</b></span>
-                <span className='location-card'><b>{props.item.location}</b></span>
+                <span className='location-name'><b>{props.item.location}</b></span>
                 <span className='date-card'><b>{props.item.date}</b></span>
                 <span className='desc-card'><b>{props.item.description}</b></span>
             </div>
